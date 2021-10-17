b88f33fc09b32d9154f9ca4dd2e01b2974163551
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-10-17 16:49:07 +0300

    use state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React , {useState} from "react";
+//import styles
 import './styles/app.scss';
 //Adding Components
 import Player from './componnents/Player'
 import Song from './componnents/Song'
+//import util
+import data from './util'
 
 function App() {
+  //state 
+  const [songs , setSongs ] = useState(data());
+  const [ currentSong , steCurrentSong] =useState(songs[0]);
   return (
     <div className="App">
-     <Song />
+     <Song currentSong={currentSong} />
      <Player />
     </div>
   );
