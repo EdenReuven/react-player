b133fed45e14c8f88dee3ca5008f80e303f154ad
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-10-20 13:08:53 +0300

    library toggle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles/app.scss';
 import Player from './componnents/Player'
 import Song from './componnents/Song'
 import Library from "./componnents/Library";
+import Nav from "./componnents/Nav";
 //import util
 import data from './util'
 
@@ -19,6 +20,7 @@ function App() {
     currentTime:0,
     duration:0,
   });
+  const [libraryStatus, setLibraryStatus] =useState(false);
   const timeUpdateHandler = (e) =>{
     const current=e.target.currentTime;
     const duration = e.target.duration;
@@ -27,22 +29,27 @@ function App() {
   }
   return (
     <div className="App">
+      <Nav 
+        libraryStatus ={libraryStatus}
+        setLibraryStatus={setLibraryStatus}
+      />
      <Song currentSong={currentSong} />
      <Player
-      audioRef ={audioRef}
-      setIsPlaying={setIsPlaying}
-      isPlaying={isPlaying} 
-      currentSong={currentSong} 
-      setSongInfo={setSongInfo}
-      songInfo={songInfo}
-    />
-    <Library 
-      audioRef={audioRef} 
-      songs={songs} 
-      setCurrentSong ={setCurrentSong}
-      isPlaying={isPlaying}
-      setSongs={setSongs}
-    />
+        audioRef ={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying} 
+        currentSong={currentSong} 
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+      />
+      <Library 
+        audioRef={audioRef} 
+        songs={songs} 
+        setCurrentSong ={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus = {libraryStatus}
+      />
 
     <audio
       onTimeUpdate={timeUpdateHandler} 
