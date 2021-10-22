54b30e5bd1a2401ebed29acbe4f247f8c53de61f
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-10-22 05:02:34 +0300

    auto skip

--- src/App.js
+++ src/App.js
@@ -29,10 +29,15 @@ function App() {
     const roundedCurrent = Math.round(current);
     const roundedDuration = Math.round(duration);
     const animation = Math.round((roundedCurrent / roundedDuration) *100)
-    setSongInfo({...songInfo, currentTime:current ,duration , animationPercentage:animation})
+    setSongInfo({...songInfo, currentTime:current ,duration , animationPercentage:animation});
 
+  };
+  const songEndHandler = async () =>{
+    let currentIndex = songs.findIndex((song)=> song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if(isPlaying) audioRef.current.play();
   }
-  return (
+    return (
     <div className="App">
       <Nav 
         libraryStatus ={libraryStatus}
@@ -64,6 +69,7 @@ function App() {
       onLoadedMetadata={timeUpdateHandler} 
       ref={audioRef} 
       src={currentSong.audio}
+      onEnded={songEndHandler}
     ></audio>
     </div>
   );
